c598213158d8e920432fdcc8d3fd317cfae1c98b
Derrick Oswald <derrick.oswald@gmail.com>
2016-09-06 09:18:22 +0200

    GridLAB-D inception

---
+++ GridLAB-D/src/main/scala/ch/ninecode/GridLABDApplication.scala
@@ -0,0 +1,13 @@
+package ch.ninecode;
+
+class GridLABDApplication 
+{
+}
+
+object GridLABDApplication
+{
+    def main (args: Array[String])
+    {
+        println ("hello world")
+    }
+}
---
+++ GridLAB-D/src/test/scala/ch/ninecode/GridLABDTestSuite.scala
@@ -0,0 +1,57 @@
+package ch.ninecode
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.storage.StorageLevel
+
+import org.scalatest.fixture
+
+import ch.ninecode._
+import ch.ninecode.cim._
+import ch.ninecode.model._
+
+class GridLABDTestSuite extends fixture.FunSuite
+{
+    case class ContextPair (_SparkContext: SparkContext, _SQLContext: SQLContext)
+
+    val FILE_DEPOT = "/home/derrick/Documents/9code/nis/cim/cim_export/"
+
+    type FixtureParam = ContextPair
+
+    def withFixture (test: OneArgTest): org.scalatest.Outcome =
+    {
+        // create the fixture
+
+        // create the configuration
+        val configuration = new SparkConf (false)
+        configuration.setAppName ("ShortCircuitSuite")
+        configuration.setMaster ("local[2]")
+        configuration.set ("spark.driver.memory", "1g")
+        configuration.set ("spark.executor.memory", "4g")
+
+        // register low level classes
+        configuration.registerKryoClasses (Array (classOf[Element], classOf[BasicElement], classOf[Unknown]))
+        // register CIM case classes
+        CHIM.apply_to_all_classes { x => configuration.registerKryoClasses (Array (x.runtime_class)) }
+        // register edge related classes
+        configuration.registerKryoClasses (Array (classOf[PreEdge], classOf[Extremum], classOf[Edge]))
+        // ToDo: register GridLAB-D classes
+
+        val context = new SparkContext (configuration)
+        context.setLogLevel ("INFO") // Valid log levels include: ALL, DEBUG, ERROR, FATAL, INFO, OFF, TRACE, WARN
+        val sql_context = new SQLContext (context)
+        try
+        {
+            withFixture (test.toNoArgTest (ContextPair (context, sql_context))) // "loan" the fixture to the test
+        }
+        finally context.stop () // clean up the fixture
+    }
+
+    test ("Basic")
+    {
+        a: ContextPair ⇒
+
+        assert(1==1)
+    }
+}
