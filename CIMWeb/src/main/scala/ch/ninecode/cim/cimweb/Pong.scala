e1364bab5870e02e9ea59707c256d35aa54bf25b
Derrick Oswald <derrick.oswald@gmail.com>
2017-08-25 15:05:13 +0200

    add logging

--- CIMWeb/src/main/scala/ch/ninecode/cim/cimweb/Pong.scala
+++ CIMWeb/src/main/scala/ch/ninecode/cim/cimweb/Pong.scala
@@ -8,16 +8,26 @@ import javax.ws.rs.Path
 import javax.ws.rs.Produces
 
 import java.util.Date
+import java.util.logging.Logger
 
 @Stateless
 @Path ("/pong")
 class Pong extends RESTful
 {
+    import Pong._
+
     @GET
     @Produces (Array (MediaType.APPLICATION_JSON))
     def pong (): String =
     {
         val date = new Date ().toString ()
+        _Logger.info ("pong @ %s".format (date))
         return (new RESTfulResult ("OK", date).toString ())
     }
+}
+
+object Pong
+{
+    val LOGGER_NAME = Pong.getClass.getName ()
+    val _Logger = Logger.getLogger (LOGGER_NAME)
 }
