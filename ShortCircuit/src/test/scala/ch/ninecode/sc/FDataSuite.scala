8381eebe08d9ad7c4cc3345491acca24de55141e
junm <markus.jung@nis.ch>
2018-09-26 07:36:53 +0200

    fix unit test caused of changed type

--- ShortCircuit/src/test/scala/ch/ninecode/sc/FDataSuite.scala
+++ ShortCircuit/src/test/scala/ch/ninecode/sc/FDataSuite.scala
@@ -36,20 +36,23 @@ class FDataSuite extends FunSuite
 
     test ("FuseOK")
     {
+        val list = List ( List (("TEI124", 288282.0), ("TEI123", 73737.3)), List(("TEI134", 1323.8), ("TEI135", 100.0)), List(("TEI141", 40.0)))
         number: Int ⇒
-            assert (FData.fuseOK (123.456, List (288282.0, 73737.3, 1323.8, 100.0, 40.0)), "expected OK")
+            assert (FData.fuseOK (123.456, list), "expected OK")
     }
 
     test ("FuseNotOK")
     {
-        number: Int ⇒
-            assert (!FData.fuseOK (123.456, List (288282.0, 73737.3, 1323.8, 50.0)), "expected not OK")
+        val list = List ( List (("TEI12", 288282.0 )), List (("TEI13", 73737.3)), List(("TEI14", 1323.8)), List(("TEI145", 50.0)))
+        number: Int ⇒ 
+            assert (!FData.fuseOK (123.456, list), "expected not OK")
     }
 
     test ("Fuse0")
     {
+        val list = List ( List (("TEI12", 288282.0)), List(("TEI13", 73737.3)), List(("TEIA14", 1323.8)), List(("TEI14", 100.0)), List(("TEI15", 40.0)), List(("TEI16", 0.0)))
         number: Int ⇒
-            assert (FData.fuseOK (123.456, List (288282.0, 73737.3, 1323.8, 100.0, 40.0, 0.0)), "expected OK")
+            assert (FData.fuseOK (123.456, list), "expected OK")
     }
 
 }
