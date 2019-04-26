11c78ae1b4b9817f0a2e2fa64d89cc530a2b9140
Derrick Oswald <derrick.oswald@9code.ch>
2019-04-26 15:06:11 +0200

    hostname tomee

--- start-docker.cmd
+++ start-docker.cmd
@@ -6,4 +6,4 @@ SET network=--network spark_default
 SET cassandra=%2
 
 
-docker run --name tomee --rm %network% --env HDFS_USER=%user% --env HADOOP_USER_NAME=%USERNAME% --publish 9080:9080 derrickoswald/cimapplication start-tomee %master% %cassandra%
+docker run --name tomee --hostname tomee --rm %network% --env HDFS_USER=%user% --env HADOOP_USER_NAME=%USERNAME% --publish 9080:9080 derrickoswald/cimapplication start-tomee %master% %cassandra%
