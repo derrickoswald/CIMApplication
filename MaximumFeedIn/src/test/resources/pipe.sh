98e651eab49cb442b96289691255a4c0878a4f79
Derrick Oswald <derrick.oswald@9code.ch>
2020-01-08 16:48:24 +0100

    use workdir in Windows pipe.sh workaround

--- MaximumFeedIn/src/test/resources/pipe.sh
+++ MaximumFeedIn/src/test/resources/pipe.sh
@@ -2,7 +2,7 @@
 while read line; do
     export FILE=${line/$'\r'/};
     ulimit -Sn `ulimit -Hn`;
-    pushd simulation/$FILE > /dev/null;
+    pushd $1/$FILE > /dev/null;
     gridlabd $FILE.glm 2>&1 | awk '{print ENVIRON["FILE"] " " $0}' > $FILE.out;
     cat output_data/* > output.txt;
     cat $FILE.out;
