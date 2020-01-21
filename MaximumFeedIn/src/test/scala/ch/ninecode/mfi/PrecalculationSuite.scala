55bf4dcfa774ba592b6c9ad0d57bb88b7f38f1a0
Derrick Oswald <derrick.oswald@9code.ch>
2020-01-21 13:07:38 +0100

    Add test to ensure multiple cables to an EnergyConsumer from separate transformers of a meshed network are handled by the heuristic limit.

--- MaximumFeedIn/src/test/scala/ch/ninecode/mfi/PrecalculationSuite.scala
+++ MaximumFeedIn/src/test/scala/ch/ninecode/mfi/PrecalculationSuite.scala
@@ -28,6 +28,7 @@ class PrecalculationSuite extends fixture.FunSuite with BeforeAndAfter
     val FILE_DEPOT = "data/"
     val FILENAME1 = "multipleconductor"
     val FILENAME2 = "multipletransformer"
+    val FILENAME3 = "meshednetwork"
 
     def using[T <: AutoCloseable, R] (resource: T)(block: T => R): R =
     {
@@ -126,12 +127,15 @@ class PrecalculationSuite extends fixture.FunSuite with BeforeAndAfter
             new Unzip ().unzip (s"$FILE_DEPOT$FILENAME1.zip", FILE_DEPOT)
         if (!new File (s"$FILE_DEPOT$FILENAME2.rdf").exists)
             new Unzip ().unzip (s"$FILE_DEPOT$FILENAME2.zip", FILE_DEPOT)
+        if (!new File (s"$FILE_DEPOT$FILENAME3.rdf").exists)
+            new Unzip ().unzip (s"$FILE_DEPOT$FILENAME3.zip", FILE_DEPOT)
     }
 
     after
     {
         new File (s"$FILE_DEPOT$FILENAME1.rdf").delete
         new File (s"$FILE_DEPOT$FILENAME2.rdf").delete
+        new File (s"$FILE_DEPOT$FILENAME3.rdf").delete
     }
 
     def withFixture (test: OneArgTest): org.scalatest.Outcome =
@@ -229,7 +233,7 @@ class PrecalculationSuite extends fixture.FunSuite with BeforeAndAfter
     }
 
     /**
-     * Test for correct calculation of heuristic limit for parallel cables.
+     * Test for correct calculation of heuristic limit for multiple transformers.
      */
     test ("MultipleTransformers")
     {
@@ -274,5 +278,56 @@ class PrecalculationSuite extends fixture.FunSuite with BeforeAndAfter
             assert (node.details == "transformer limit")
             // 2 x 100kVA = 200000
             assert (near (node.max_power_feeding, 200000, 1.0))
+    }
+
+    /**
+     * Test for correct calculation of heuristic limit for meshed networks.
+     */
+    test ("MeshedNetwork")
+    {
+        session: SparkSession ⇒
+
+            val begin = System.nanoTime ()
+
+            val filename = s"$FILE_DEPOT$FILENAME3.rdf"
+            readFile (session, filename)
+
+            val read = System.nanoTime ()
+            println ("read : " + (read - begin) / 1e9 + " seconds")
+
+            // set up for execution
+            val gridlabd = new GridLABD (session = session, topological_nodes = true, one_phase = true, workdir = ".")
+            val storage_level = StorageLevel.MEMORY_AND_DISK_SER
+
+            // do all low voltage power transformers
+            val _transformers = new Transformers (session, storage_level)
+            val transformer_data = _transformers.getTransformers ()
+            val transformers = transformer_data.groupBy (_.node1.TopologicalIsland).values.map (TransformerIsland.apply)
+            transformers.persist (storage_level).name = "Transformers"
+
+            // construct the initial graph from the real edges and nodes
+            val (xedges, xnodes) = gridlabd.prepare ()
+            val initial = Graph.apply [PreNode, PreEdge](xnodes, xedges, PreNode ("", 0.0, null), storage_level, storage_level)
+
+            // get the existing photo-voltaic installations keyed by terminal
+            val solar = Solar (session, topologicalnodes = true, storage_level)
+            val sdata = solar.getSolarInstallations
+
+            val power_feeding = new PowerFeeding (session)
+            val results: PreCalculationResults = power_feeding.threshold_calculation (initial, sdata, transformers, EinspeiseleistungOptions ())
+            val houses = results.has
+            // println (houses.take (100).mkString ("\n"))
+            val has = houses.filter (_.mrid == "USR0001")
+            assert (!has.isEmpty)
+            val nodes = has.take (10)
+            assert (nodes.length == 1)
+            val node = nodes(0)
+            assert (node.reason == "heuristic limit")
+            assert (node.details == "limitation of last cable(s)")
+            // two cables from two different transformers:
+            // GKN 3x10re/10 1/0.6 kV with ratedCurrent 67A, @ (400 + 1.97)V * √3 = 46648
+            // GKN 3x16rm/16 1/0.6 kV with ratedCurrent 88A, @ (400 + 2.58)V * √3 = 61361
+            //                                                                     108009
+            assert (near (node.max_power_feeding, 108009, 1.0))
     }
 }
