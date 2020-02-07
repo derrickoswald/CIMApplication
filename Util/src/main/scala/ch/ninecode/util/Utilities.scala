63e57e0b2acc98898d96f841db1028eb99e92959
junm <markus.jung@nis.ch>
2020-02-07 14:00:24 +0100

    add util module for common functions

---
+++ Util/src/main/scala/ch/ninecode/util/Utilities.scala
@@ -0,0 +1,93 @@
+package ch.ninecode.util
+
+import java.io.BufferedOutputStream
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.zip.ZipInputStream
+
+trait Using {
+    def using[T <: AutoCloseable, R] (resource: T)(block: T => R): R =
+    {
+        try
+        {
+            block (resource)
+        }
+        finally
+        {
+            resource.close ()
+        }
+    }
+}
+
+trait Unzip {
+    /**
+     * This utility extracts files and directories of a standard zip file to
+     * a destination directory.
+     *
+     * @author www.codejava.net
+     *
+     */
+    class Unzip extends Using
+    {
+        /**
+         * Extracts a zip file specified by the file to a directory.
+         *
+         * The directory will be created if does not exist.
+         *
+         * @param file      The Zip file.
+         * @param directory The directory to extract it to
+         * @throws IOException If there is a problem with the zip extraction
+         */
+        @throws[IOException]
+        def unzip (file: String, directory: String): Unit =
+        {
+            val dir = new File (directory)
+            if (!dir.exists)
+                dir.mkdir
+            using (new ZipInputStream (new FileInputStream (file)))
+            {
+                zip =>
+                    var entry = zip.getNextEntry
+                    // iterates over entries in the zip file
+                    while (null != entry)
+                    {
+                        val path = directory + entry.getName
+                        if (!entry.isDirectory)
+                        // if the entry is a file, extract it
+                            extractFile (zip, path)
+                        else
+                        // if the entry is a directory, make the directory
+                            new File (path).mkdir
+                        zip.closeEntry ()
+                        entry = zip.getNextEntry
+                    }
+            }
+        }
+
+        /**
+         * Extracts a zip entry (file entry).
+         *
+         * @param zip  The Zip input stream for the file.
+         * @param path The path to extract he file to.
+         * @throws IOException If there is a problem with the zip extraction
+         */
+        @throws[IOException]
+        private def extractFile (zip: ZipInputStream, path: String): Unit =
+        {
+            val bytesIn = new Array[Byte](4096)
+            using (new BufferedOutputStream (new FileOutputStream (path)))
+            {
+                bos =>
+                    var read = -1
+                    while (
+                    {
+                        read = zip.read (bytesIn)
+                        read != -1
+                    })
+                        bos.write (bytesIn, 0, read)
+            }
+        }
+    }
+}
