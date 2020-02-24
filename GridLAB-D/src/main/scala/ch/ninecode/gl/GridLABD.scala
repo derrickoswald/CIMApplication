b07f7adcf4c7964a3de2542d605303b38c286ac6
sapl <luca.saputelli.@nis.ch>
2020-02-24 09:23:31 +0100

    Fix gridlabd bash script

--- GridLAB-D/src/main/scala/ch/ninecode/gl/GridLABD.scala
+++ GridLAB-D/src/main/scala/ch/ninecode/gl/GridLABD.scala
@@ -479,7 +479,7 @@ class GridLABD
                             "pushd " + workdir_path + "$FILE; " +
                             "gridlabd --quiet $FILE.glm 2> $FILE.out;" +
                             "cat output_data/* > output.txt; " +
-                            "echo -n $FILE|;" +
+                            "echo -n $FILE'|';" +
                             "cat $FILE.out | tr '\r\n' '|';" +
                             "popd; " +
                             "done < /dev/stdin")
@@ -502,7 +502,7 @@ class GridLABD
                         "popd; " +
                         "$HDFS_DIR/bin/hdfs dfs -copyFromLocal -f $FILE/output.txt " + workdir_path + "$FILE; " +
                         "$HDFS_DIR/bin/hdfs dfs -copyFromLocal -f $FILE/$FILE.out " + workdir_path + "$FILE/$FILE.out; " +
-                        "echo -n $FILE|;" +
+                        "echo -n $FILE'|';" +
                         "cat $FILE.out | tr '\r\n' '|';" +
                         "rm -rf $FILE; " +
                         "done < /dev/stdin")
--- GridLAB-D/src/test/resources/pipe.sh
+++ GridLAB-D/src/test/resources/pipe.sh
@@ -5,7 +5,7 @@ while read line; do
     pushd $1/$FILE > /dev/null;
     gridlabd.exe $FILE.glm 2> $FILE.out;
     cat output_data/* > output.txt;
-    echo -n "$FILE|";
+    echo -n $FILE'|';
     cat $FILE.out | tr '\r\n' '|';
     popd > /dev/null;
 done
