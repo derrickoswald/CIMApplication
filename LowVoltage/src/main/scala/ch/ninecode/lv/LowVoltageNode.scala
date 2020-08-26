32e75b9b7dc4b945672211bc3da48d4cf5dab58e
Derrick Oswald <derrick.oswald@9code.ch>
2020-08-26 17:10:48 +0200

    Use island processing instead of MaximumFeedIn tracing
    Remove the dependency on MaximumFeedIn, which simplifies things and doesn't generate a bogus results.db.
    Use Net module to determine transformer service areas.
    Stop trying to read the old cim16 FullModel header information to determine dates.
    Stop trying to rename the output directory to contain the date.

---
+++ LowVoltage/src/main/scala/ch/ninecode/lv/LowVoltageNode.scala
@@ -0,0 +1,24 @@
+package ch.ninecode.lv
+
+import ch.ninecode.gl.GLMNode
+import ch.ninecode.net.LoadFlowNode
+
+/**
+ * Vertex data.
+ *
+ * @param id                 TopologicalNode mRID
+ * @param nominal_voltage    node voltage (V)
+ * @param equipment          ConductingEquipment mRID
+ * @param isEnergyConsumer   <code>true</code> if this node is an EnergyConsumer
+ */
+case class LowVoltageNode
+(
+    override val id: String,
+    override val nominal_voltage: Double,
+    equipment: String,
+    isEnergyConsumer: Boolean
+)
+extends LoadFlowNode (id, nominal_voltage) with GLMNode
+{
+    override def toString: String = s"$id $equipment (${nominal_voltage}V)${ if (isEnergyConsumer) " EnergyConsumer" else "" }"
+}
