d734d4d58cccca81b25feafd731c48d54be8ec73
Derrick Oswald <derrick.oswald@9code.ch>
2020-08-30 13:14:58 +0200

    DocComment

--- Simulation/src/main/scala/ch/ninecode/sim/SimulationEdge.scala
+++ Simulation/src/main/scala/ch/ninecode/sim/SimulationEdge.scala
@@ -7,6 +7,7 @@ import ch.ninecode.net.LoadFlowEdge
 /**
  * Edge data.
  *
+ * @param rawedge            The underlying edge object.
  * @param world_position     The array of (x,y) coordinates of the PositionPoints of the ConductingEquipment in world coordinates, or <code>null</code> if none.
  * @param schematic_position The array of (x,y) coordinates of the PositionPoints of the ConductingEquipment in schematic coordinates, or <code>null</code> if none.
  * @param players            Players attached to this edge - if any.
