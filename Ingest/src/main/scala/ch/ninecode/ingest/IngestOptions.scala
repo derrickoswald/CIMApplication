5435a5a8e88538aadfd9dcbef374dc54c63f1f9c
Derrick Oswald <derrick.oswald@9code.ch>
2020-09-02 21:09:06 +0200

    Update Ingest to new options and main() paradigm
        Need to keep the host:port in sync in the IngestOptions and SparkOptions.

--- Ingest/src/main/scala/ch/ninecode/ingest/IngestOptions.scala
+++ Ingest/src/main/scala/ch/ninecode/ingest/IngestOptions.scala
@@ -1,39 +1,29 @@
 package ch.ninecode.ingest
 
-object LogLevels extends Enumeration
-{
-    type LogLevels = Value
-    val ALL, DEBUG, ERROR, FATAL, INFO, OFF, TRACE, WARN = Value
-}
+import ch.ninecode.util.MainOptions
+import ch.ninecode.util.Mainable
+import ch.ninecode.util.SparkOptions
+import ch.ninecode.util.Sparkable
 
 /**
  * Ingest meter readings options.
  *
- * @param valid     False if either help or version requested (i.e. don't proceed with execution).
- * @param unittest  If <code>true</code>, don't call sys.exit().
- * @param verbose   If <code>true</code>, emit progress messages.
- * @param master    Spark master.
- * @param options   Spark options.
- * @param host      Cassandra connection host.
- * @param port      Cassandra connection port.
- * @param storage   Storage level for RDD serialization.
- * @param log_level Logging level.
- * @param workdir   Working directory for unzipping and copying if nocopy is <code>false</code>.
+ * @param verbose    If <code>true</code>, emit progress messages.
+ * @param host       Cassandra connection host.
+ * @param port       Cassandra connection port.
+ * @param workdir    Working directory for unzipping and copying if nocopy is <code>false</code>.
+ * @param ingestions File to ingest.
  */
 case class IngestOptions
 (
-    var valid: Boolean = true,
-    unittest: Boolean = false,
+    var main_options: MainOptions = MainOptions (),
+    var spark_options: SparkOptions = SparkOptions (),
     verbose: Boolean = false,
-    master: String = "local[*]",
-    options: Map[String, String] = Map (),
     host: String = "localhost",
     port: Int = 9042,
-    storage: String = "MEMORY_AND_DISK_SER",
-    log_level: LogLevels.Value = LogLevels.OFF,
     workdir: String = s"${IngestOptions.cwd}/work/",
     ingestions: Seq[String] = Seq ()
-)
+) extends Mainable with Sparkable
 
 object IngestOptions
 {
