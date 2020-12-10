303148606b159e893684910790edf2e152519a46
sapl <luca.saputelli.@nis.ch>
2020-12-10 10:47:39 +0100

    WIK-1436: Change version for release

--- Util/src/main/scala/ch/ninecode/util/MainOptions.scala
+++ Util/src/main/scala/ch/ninecode/util/MainOptions.scala
@@ -11,7 +11,7 @@ package ch.ninecode.util
 case class MainOptions
 (
     application: String = "SparkApplication",
-    version: String = "2.12-3.0.1-3.0.4",
+    version: String = "2.12-3.0.1-3.0.5",
     valid: Boolean = true,
     unittest: Boolean = false,
 )
