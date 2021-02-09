5ca7334679dea7b1a003744c582b5846f9e38cb8
junm <markus.jung@nis.ch>
2021-02-09 09:24:04 +0100

    Revert "WIK-1534: group trafosets by topo island"
    This reverts commit 6f431fa4

--- Net/src/main/scala/ch/ninecode/net/TransformerIsland.scala
+++ Net/src/main/scala/ch/ninecode/net/TransformerIsland.scala
@@ -48,7 +48,7 @@ object TransformerIsland
     def apply (transformers: Iterable[TransformerData]): TransformerIsland =
     {
         val sets = transformers
-            .groupBy(t => s"${t.node1.TopologicalIsland}")
+            .groupBy(t => s"${t.node1}")
             .map(set => TransformerSet(set._2.toArray))
             .toArray
         TransformerIsland(sets)
