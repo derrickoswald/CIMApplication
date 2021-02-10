ec3b3f2722b2717ad2ec39d3ec1c6fc2394e5c25
junm <markus.jung@nis.ch>
2021-02-10 11:30:42 +0100

    WIK-1534: (refactoring) create its own class for branch reduction

--- ShortCircuit/src/main/scala/ch/ninecode/sc/ShortCircuit.scala
+++ ShortCircuit/src/main/scala/ch/ninecode/sc/ShortCircuit.scala
@@ -459,140 +459,6 @@ case class ShortCircuit (session: SparkSession, storage_level: StorageLevel, opt
         files.map(extract_trafo).flatMapValues(read)
     }
 
-    /**
-     * Reduce series connected elements.
-     *
-     * @param network     the current network to be reduced
-     * @param trafo_nodes the list of starting trafo nodes
-     * @return the reduced network with one pair of series elements converted to a series branch
-     */
-    def reduce_series (network: Iterable[Branch], trafo_nodes: Array[String], mrid: String): (Boolean, Iterable[Branch]) = // (reduced?, network)
-    {
-        // check for series elements, eliminate making a series connection across the house or trafo
-        val prepend =
-            for
-                {
-                branch <- network
-                house = branch.from == mrid
-                if !house
-                trafo = trafo_nodes.contains(branch.from)
-                if !trafo
-                buddies = network.filter(x => (branch.from == x.to) || (branch.from == x.from && branch != x))
-                if buddies.size == 1
-                buddy :: _ = buddies
-            }
-                yield (branch, buddy)
-
-        val append =
-            for
-                {
-                branch <- network
-                house = branch.to == mrid
-                if !house
-                trafo = trafo_nodes.contains(branch.to)
-                if !trafo
-                buddies = network.filter(x => (branch.to == x.from) || (branch.to == x.to && branch != x))
-                if buddies.size == 1
-                buddy :: _ = buddies
-            }
-                yield (branch, buddy)
-
-        val series = prepend ++ append
-
-        series match
-        {
-            case (branch, buddy) :: _ =>
-                // only do one reduction at a time... I'm not smart enough to figure out how to do it in bulk
-                val rest = network.filter(x => branch != x && buddy != x)
-                val new_series = if (buddy.to == branch.from)
-                    buddy.add_in_series(branch)
-                else
-                    if (branch.to == buddy.from)
-                        branch.add_in_series(buddy)
-                    else
-                    // choose the simplest element to reverse
-                        branch match
-                        {
-                            case _: SimpleBranch =>
-                                buddy.add_in_series(branch.reverse)
-                            case _: TransformerBranch =>
-                                buddy.add_in_series(branch.reverse)
-                            case _ =>
-                                buddy.reverse.add_in_series(branch)
-                        }
-                (true, Seq(new_series) ++ rest)
-            case _ =>
-                (false, network)
-        }
-    }
-
-    /**
-     * Reduce parallel connected elements.
-     *
-     * @param network the current network to be reduced
-     * @return the reduced network with one pair of parallel elements converted to a parallel branch
-     */
-    @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
-    def reduce_parallel (network: Iterable[Branch]): (Boolean, Iterable[Branch]) = // (reduced?, network)
-    {
-        // check for parallel elements
-        val parallel = for
-            {
-            branch <- network
-            buddies = network.filter(x => ((branch.from == x.from) && (branch.to == x.to)) && (branch != x))
-            if buddies.nonEmpty
-        }
-            yield buddies ++ Seq(branch)
-        parallel match
-        {
-            case set :: _ =>
-                // only do one reduction at a time... I'm not smart enough to figure out how to do it in bulk
-                (true, Seq(set.head.add_in_parallel(set.tail)) ++ network.filter(x => !set.toSeq.contains(x)))
-            case _ =>
-                (false, network)
-        }
-    }
-
-    def reduce (branches: Iterable[Branch], trafo_nodes: Array[String], mrid: String): Iterable[Branch] =
-    {
-        // step by step reduce the network to a single branch through series and parallel reductions
-        var done = false
-        var network: Iterable[Branch] = branches
-        do
-        {
-            val (modified, net) = reduce_series(network, trafo_nodes, mrid)
-            network = net
-            done = !modified
-            if (done)
-            {
-                val (modified, net) = reduce_parallel(network)
-                network = net
-                done = !modified
-                // check that all branches start from the transformer
-                if (done)
-                    if (!network.forall(x => trafo_nodes.contains(x.from)))
-                    {
-                        val max = network.map(_.current).foldLeft(Double.MinValue)((x: Double, y: Double) => if (x > y) x else y)
-                        val significant = max * 0.01 // 1% of the maximum current
-                        val filtered = network.filter(x =>
-                            (x.current > significant)
-                                || trafo_nodes.contains(x.from)
-                                || trafo_nodes.contains(x.to)
-                                || (x.from == mrid)
-                                || (x.to == mrid))
-                        if (filtered.size < network.size)
-                        {
-                            done = false
-                            network = filtered
-                        }
-                    }
-            }
-        }
-        while (!done)
-
-        network
-    }
-
     @SuppressWarnings(Array("org.wartremover.warts.TraversableOps"))
     def makeCableBranch (cable: GLMLineEdge, voltage1: ThreePhaseComplexDataElement, voltage2: ThreePhaseComplexDataElement, v1: Double, v2: Double): List[Branch] =
     {
@@ -724,44 +590,11 @@ case class ShortCircuit (session: SparkSession, storage_level: StorageLevel, opt
             }
         )
 
-        def connectedTo (branch: Branch, edges: Iterable[Branch]): Boolean =
-        {
-            edges.exists(edge => edge != branch && (branch.to == edge.to || branch.to == edge.from))
-        }
+        val branches = new ScBranches().reduce_branches(graph_edges, lvnodes, experiment)
 
-        def connectedFrom (branch: Branch, edges: Iterable[Branch]): Boolean =
-        {
-            edges.exists(edge => edge != branch && (branch.from == edge.to || branch.from == edge.from))
-        }
-
-        // eliminate branches in the tree than only have one end connected - except for the starting and ending node
-        def no_stubs (edges: Iterable[Branch], start: Array[String], end: String)(branch: Branch): Boolean =
-        {
-            val to = connectedTo(branch, edges)
-            val from = connectedFrom(branch, edges)
-            val connected = to && from
-            val into = start.contains(branch.to)
-            val infrom = start.contains(branch.from)
-
-            connected ||
-                (end == branch.to && (infrom || from)) ||
-                (end == branch.from && (into || to)) ||
-                (infrom && to) ||
-                (into && from)
-        }
-
-        // reduce the tree to (hopefully) one branch spanning from start to end
-        var family = graph_edges
-        var count = family.size
-        do
-        {
-            count = family.size
-            family = family.filter(no_stubs(family, lvnodes, experiment.mrid))
-        }
-        while (count != family.size)
-        val branches = reduce(family, lvnodes, experiment.mrid)
-        // ToDo: this will need to be revisited for mesh networks where there are multiple supplying transformers - finding the first is not sufficient
-        val twig = branches.find(
+        // ToDo: this will need to be revisited for mesh networks where there are multiple supplying transformers
+        // finding the first is not sufficient
+        val twig: Option[Branch] = branches.find(
             branch =>
                 ((experiment.mrid == branch.to) && lvnodes.contains(branch.from)) ||
                     ((experiment.mrid == branch.from) && lvnodes.contains(branch.to))
@@ -1334,6 +1167,7 @@ object ShortCircuit extends CIMInitializer[ShortCircuitOptions] with Main
                     if (options.verbose)
                     {
                         org.apache.log4j.LogManager.getLogger("ch.ninecode.sc.ShortCircuit").setLevel(org.apache.log4j.Level.INFO)
+                        org.apache.log4j.LogManager.getLogger("ch.ninecode.sc.ScBranches").setLevel(org.apache.log4j.Level.INFO)
                         org.apache.log4j.LogManager.getLogger("ch.ninecode.sc.Database").setLevel(org.apache.log4j.Level.INFO)
                     }
                     execute(options)
