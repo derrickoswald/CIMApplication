72c451647d8a1927135a4abd512ec69d8356497d
junm <markus.jung@nis.ch>
2021-08-05 07:08:26 +0200

    IWT-995: release: adjust version number

--- Util/src/main/scala/ch/ninecode/util/MainOptions.scala
+++ Util/src/main/scala/ch/ninecode/util/MainOptions.scala
@@ -11,7 +11,7 @@ package ch.ninecode.util
 case class MainOptions
 (
     application: String = "SparkApplication",
-    version: String = "2.12-3.0.1-3.0.8",
+    version: String = "2.12-3.0.1-3.0.9",
     valid: Boolean = true,
     unittest: Boolean = false,
 )
